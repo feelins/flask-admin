ffad520804bc8a30f9a2e438df1b70027242e6b9
feelins <feipengshao@163.com>
2022-12-18 14:39:23 +0800

    增加读取excel表格

--- examples/custom-layout/app.py
+++ examples/custom-layout/app.py
@@ -16,7 +16,7 @@ app.app_context().push()
 app.config['SECRET_KEY'] = '123456790'
 
 # Create in-memory database
-app.config['DATABASE_FILE'] = r'E:\003_ProgramLanguage\flask-admin\examples\custom-layout\sample_db.sqlite'
+app.config['DATABASE_FILE'] = r'sample_db.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
@@ -26,20 +26,19 @@ db = SQLAlchemy(app)
 class Information(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     lang = db.Column(db.Unicode(64))
-    gender = db.Column(db.Unicode(8))
+    score = db.Column(db.Unicode(64))
     name = db.Column(db.Unicode(64))
     duration = db.Column(db.Unicode(24))
-    engine_name_id = db.Column(db.Unicode(64), unique=True)
     update_date = db.Column(db.DateTime, default=pd.to_datetime("today"))
 
     def __unicode__(self):
         return self.name
 
 
-class Evaluation(db.Model):
+class ScoreRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     lang = db.Column(db.Unicode(64))
-    mos_type = db.Column(db.Unicode(64))
+    score = db.Column(db.Unicode(64))
     update_date = db.Column(db.DateTime, default=pd.to_datetime("today"))
     # article = db.relationship("Information",backref='speaker_style',lazy='dynamic')
     """
@@ -49,15 +48,6 @@ class Evaluation(db.Model):
         subquery: 与joined类似，但使用子子查询
         dynamic: 不加载记录，但提供加载记录的查询，也就是生成query对象
     """
-
-    def __unicode__(self):
-        return self.name
-
-
-class Version(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    other_comment = db.Column(db.Text)
-    update_time = db.Column(db.DateTime, default=pd.to_datetime("today"))
 
     def __unicode__(self):
         return self.name
@@ -78,36 +68,32 @@ class CustomView(ModelView):
     edit_template = 'edit.html'
 
 
-class EvaluationAdmin(CustomView):
+class ScoreRecordAdmin(CustomView):
     can_export = True
     can_delete = False
-    column_list = ('lang', 'mos_type', 'update_date')
-    column_searchable_list = ('lang', 'mos_type', 'update_date')
-    column_filters = ('lang', 'mos_type', 'update_date')
+    column_list = ('lang', 'score', 'update_date')
+    column_searchable_list = ('lang', 'score', 'update_date')
+    column_filters = ('lang', 'score', 'update_date')
     can_view_details = True
     column_labels = dict(
-        lang='语言', mos_type='分类', update_date='更新日期')
-    column_editable_list = ['lang', 'mos_type', 'update_date']
-    # column_export_list = ('engine_name', 'engine_id')
+        lang='语言', score='得分', update_date='更新日期')
+    column_editable_list = ['lang', 'score', 'update_date']
 
 
 class InformationAdmin(CustomView):
     can_export = True
     can_delete = False
     # column_list不指定列，则会显示所有列
-    column_list = ('lang', 'gender', 'name', 'duration', 'engine_name_id', 'update_date')
-    column_searchable_list = ('name', 'engine_name_id', 'lang')
-    column_filters = ('name', 'engine_name_id', 'lang')
-    column_editable_list = ['lang', 'gender', 'name', 'duration', 'engine_name_id', 'update_date']
+    column_list = ('lang', 'score', 'name', 'duration', 'update_date')
+    column_searchable_list = ('name', 'lang')
+    column_filters = ('name', 'lang')
+    column_editable_list = ['lang', 'score', 'name', 'duration']
     can_view_details = True
     column_labels = dict(
-        lang='语言', gender='性别',
-        name='姓名', duration='时长',
-        engine_name_id='代号'
+        lang='语言', score='得分',
+        name='姓名', duration='时长'
     )
-    column_export_list = ('lang', 'gender', 'name', 'duration', 'engine_name_id', 'update_date')
-    # form_overrides = dict(engine_lang_name=CKEditorField)
-    # form_ajax_refs = {'engine_lang_name': ajax.QueryAjaxModelLoader('mos_acoustic_name', db.session, Evaluation,filters=["id>10"])}
+    column_export_list = ('lang', 'score', 'name', 'duration', 'update_date')
 
 
 class VersionAdmin(CustomView):
@@ -136,8 +122,7 @@ admin = admin.Admin(app, 'Example: Layout-BS3', base_template='layout.html', tem
 
 # Add views
 admin.add_view(InformationAdmin(Information, db.session))
-admin.add_view(EvaluationAdmin(Evaluation, db.session))
-admin.add_view(VersionAdmin(Version, db.session))
+admin.add_view(ScoreRecordAdmin(ScoreRecord, db.session))
 admin.add_view(AnalyticsView(name='Analytics', endpoint='analytics'))
 
 
@@ -162,33 +147,20 @@ def build_sample_db():
     db.drop_all()
     db.create_all()
 
-    input_xls = r''
+    input_xls = r'examples\custom-layout\test.xlsx'
     column_names, file_infos = load_file(input_xls, 0, 0)
 
     for item in file_infos:
         info = Information()
-        info.lang = item[column_names[2]]  #
-        info.name = item[column_names[3]]  #
-        info.gender = item[column_names[4]]  #
-        info.duration = item[column_names[5]]  #
+        info.lang = item[column_names[0]]  #
+        info.name = item[column_names[1]]  #
+        info.score = str(item[column_names[2]])  #
+        info.duration = item[column_names[3]]  #
+        info.update_date = pd.to_datetime(str(item[column_names[4]]).replace('.0', ''), format='%Y%m%d', errors='coerce')
         db.session.add(info)
 
-    input_mos_file = r''
-    column_names, file_infos = load_file(input_mos_file, 3, 1)
-
-    for i, item in enumerate(file_infos):
-        mos = Evaluation()
-        mos.lang = item[column_names[0]]  # 语种说明
-        mos.mos_type = ""
-        # pd.to_datetime(data['creatDate'], format='%Y%m%d', errors='coerce' 20221010替换为时间
-        mos.mos_date = pd.to_datetime(str(item[column_names[6]]).replace('.0', ''), format='%Y%m%d', errors='coerce')
-        mos.mos_sentence_num = str(item[column_names[7]]).replace('.0', '').replace('nan', '')
-        mos.mos_person_num = item[column_names[8]]
-        db.session.add(mos)
-
-    version = Version()
-    # version.lang = ''
-    db.session.add(version)
+    record = ScoreRecord()
+    db.session.add(record)
 
     db.session.commit()
 
@@ -202,7 +174,7 @@ if __name__ == '__main__':
     database_path = op.join(app_dir, app.config['DATABASE_FILE'])
     # print(database_path)
     # if not os.path.exists(database_path):
-    # build_sample_db()
+    build_sample_db()
     # workbook = Workbook(r'D:\001_WorkToolsFlow\flask-admin\examples\custom-layout\output.xlsx')
     # worksheet = workbook.add_worksheet()
     # # 传入数据库路径，db.s3db或者test.sqlite
